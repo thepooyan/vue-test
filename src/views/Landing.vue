db4a1bd3b87f07b778dfec4768fc799d14e0a41a
pooyan <thepooyan@gmail.com>
2025-04-13 17:48:44 +0330

    setup my own views and a link to the form page from landing

---
+++ src/views/Landing.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+  import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <main>
+    <h1>
+      <RouterLink to="/form">Go to multi step form!</RouterLink>
+    </h1>
+  </main>
+</template>
