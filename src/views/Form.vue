960a9c839de6f01a57d6e09139500a94ba08d1ae
pooyan <thepooyan@gmail.com>
2025-04-13 18:22:07 +0330

    found needed components from vutify

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -1,3 +1,29 @@
 <template>
-  form...
+  <main>
+    <h1>
+      Welcome to the form
+    </h1>
+    <p>
+      Please enter your fake phone number:
+    </p>
+    <v-text-field label="Phone number"></v-text-field>
+    <v-checkbox label="Checkbox"></v-checkbox>
+    <v-otp-input></v-otp-input>
+    <v-btn>
+      Next
+    </v-btn>
+  </main>
 </template>
+
+<style scoped>
+main {
+  border: .1em solid gray;
+  width: 35em;
+  margin: auto;
+  border-radius: .2em;
+  padding: 1em;
+  display: flex;
+  flex-direction: column;
+  gap: .5em;
+}
+</style>
