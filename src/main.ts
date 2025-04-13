7cacaeef769c6d57e4812de33c413ed1a0a49c5a
pooyan <thepooyan@gmail.com>
2025-04-13 18:02:31 +0330

    added vutify

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,8 @@
 import './assets/main.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -6,6 +10,12 @@ import router from './router'
 
 const app = createApp(App)
 
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
